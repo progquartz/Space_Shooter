29ba718077f56c08ff186bba9fc565c4fd5c601f
progquartz <progquartz@gmail.com>
2019-09-25 22:37:52 +0900

    Space_Shooter till #15

---
+++ Space_Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    public float speed;
+    private void Start()
+    { 
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * speed;
+    }
+}
